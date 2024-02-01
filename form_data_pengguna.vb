72f6c3bec7b1318d7c9a50f6d42883c6962a5186
Rafi gunawan <figun746@gmail.com>
2024-02-01 08:58:14 +0700

    mengubah kode form pengguna

--- form_data_pengguna.vb
+++ form_data_pengguna.vb
@@ -1,123 +1,117 @@
-Dim con As New ADODB.Connection
-Private id_outlet
-Public id_user_login
+Dim IdUser As Integer
+Dim Conn As New ADODB.Connection
 
-Private Sub Clear_Form()
+Private Sub cmdGadulu_Click(Index As Integer)
+    ClearForm
+End Sub
+
+Private Sub ClearForm()
     txtNama.Text = ""
-    txtId.Text = ""
-    txtPw.Text = ""
     txtUsername.Text = ""
-    Combo1.Text = "Pilih Outlet"
-    txtRole.Text = ""
+    txtEmail.Text = ""
+    txtPassword = ""
+    cmbRole.Text = "Pilih Role"
+    IdUser = 0
 End Sub
 
-Private Sub Show_Data()
-    Adodc1.ConnectionString = con
-    Adodc1.CommandType = adCmdText
-    Adodc1.RecordSource = "SELECT tb_user.id AS id_user, tb_user.nama AS nama_pengguna, username, role, tb_outlet.id AS id_outlet, tb_outlet.nama AS nama_outlet FROM tb_user INNER JOIN tb_outlet ON tb_user.id_outlet = tb_outlet.id"
-    Adodc1.Refresh
-    
-    Set DataGrid1.DataSource = Adodc1
-    DataGrid1.Columns(0).Caption = "ID"
-    DataGrid1.Columns(1).Caption = "Nama"
-    DataGrid1.Columns(2).Caption = "Username"
-    DataGrid1.Columns(3).Caption = "Role"
-    DataGrid1.Columns(4).Caption = "ID Outlet"
-    DataGrid1.Columns(5).Caption = "Nama Outlet"
-    
-End Sub
+Private Function adaYangKosong() As Boolean
+    adaYangKosong = (txtNama.Text = "" Or txtUsername.Text = "" Or txtEmail.Text = "" Or cmbRole.Text = "Pilih Role")
+End Function
 
-Private Sub Tambah_Data()
-    con.Execute "INSERT INTO tb_user (nama, username, password, role, id_outlet) VALUES ('" & txtNama & "', '" & txtUsername & "', '" & txtPw & "', '" & txtRole & "', " & id_outlet & ")"
-    
-    ' refresh data di dalam data grid
-    Adodc1.Refresh
-    
-    MsgBox "Pengguna Baru Berhasil Ditambahkan", vbInformation, "Info Bahagia"
-    
-    Call Clear_Form
-End Sub
+Private Function passwordKosong() As Boolean
+    passwordKosong = (txtPassword.Text = "")
+End Function
 
-Private Sub Update_Data()
-    If Len(Trim(txtId.Text)) <> 0 Then
-        If Len(Trim(txtPw.Text)) <> 0 Then
-            con.Execute "UPDATE tb_user SET nama = '" & txtNama & "', username = '" & txtUsername & "', password = '" & txtPw & "', role = '" & txtRole & "', id_outlet = " & id_outlet & " WHERE id = " & txtId & ";"
-        Else
-            con.Execute "UPDATE tb_user SET nama = '" & txtNama & "', username = '" & txtUsername & "', role = '" & txtRole & "', id_outlet = " & id_outlet & " WHERE id = " & txtId & ";"
-        End If
-        
-        Adodc1.Refresh
-        
-        MsgBox "Data Berhasil Di Update", vbInformation, "Info"
-        
-        Call Clear_Form
+Private Function pesanKosong()
+    MsgBox "Tolong Disi Form yang Masih Kosong!", vbInformation, "Tidak Boleh"
+End Function
+
+Private Function pesanBerhasil(pesan As String)
+    ' Kirim pesan berhasil
+    MsgBox pesan, vbInformation, "BERHASIL"
+    
+    adodcPengguna.Refresh
+    
+    ClearForm
+End Function
+
+Private Sub cmdHapus_Click(Index As Integer)
+    If IdUser = 0 Then
+        MsgBox "Silahkan Pilih Data Yang Ingin Dihapus pada Tabel Data!", vbInformation, "Informasi"
     Else
-        MsgBox "ID Tidak Boleh Kosong", vbExclamation, "Peringatan"
+        Conn.Execute "DELETE FROM tb_user WHERE id = " & IdUser
+        
+        pesanBerhasil "Data Pengguna " & txtNama.Text & " Berhasil Dihapus!"
     End If
 End Sub
 
-Private Sub Hapus_Data()
-    If txtId = id_user_login Then
-        MsgBox "Anda Tidak bisa menghapus diri sendiri", vbInformation, "Info Penting"
-    Else
-        If Len(Trim(txtId.Text)) <> 0 Then
-            con.Execute "DELETE FROM tb_user WHERE id = " & txtId & ";"
-            
-            Adodc1.Refresh
-            
-            MsgBox "Data Berhasil Dihapus", vbInformation, "Info"
-            Call Clear_Form
+Private Sub cmdTambah_Click(Index As Integer)
+    If IdUser = 0 Then
+        If adaYangKosong And passwordKosong Then
+            pesanKosong
         Else
-            MsgBox "ID Tidak Boleh Kosong", vbExclamation, "Peringatan"
+            Conn.Execute "INSERT INTO tb_user (nama, username, [password], email, role) VALUES ('" & txtNama.Text & "', '" & txtUsername.Text & "', '" & txtPassword.Text & "', '" & txtEmail.Text & "', '" & cmbRole.Text & "');"
+            
+            pesanBerhasil "Data Pengguna Baru Berhasil Ditambahkan!"
         End If
+    Else
+        MsgBox "Pengguna dengan ID " & IdUser & " Sudah Ada! (Hanya Bisa Melakukan Update/Hapus Pengguna)", vbInformation, "Tidak Bisa Bro"
     End If
 End Sub
 
-Private Sub Combo1_Click()
-    id_outlet = Combo1.ItemData(Combo1.ListIndex)
+Private Sub cmdUpdate_Click(Index As Integer)
+    If Not IdUser = 0 Then
+        If adaYangKosong Then
+            pesanKosong
+        ElseIf passwordKosong Then
+            Conn.Execute "UPDATE tb_user SET nama = '" & txtNama.Text & "', username = '" & txtUsername.Text & "', email = '" & txtEmail.Text & "', role = '" & cmbRole.Text & "' WHERE id = " & IdUser
+            pesanBerhasil "Data Pengguna " & txtNama.Text & " Berhasil Diupdate"
+        Else
+            Conn.Execute "UPDATE tb_user SET nama = '" & txtNama.Text & "', username = '" & txtUsername.Text & "', email = '" & txtEmail.Text & "', role = '" & cmbRole.Text & "', password = '" & txtPassword.Text & "' WHERE id = " & IdUser
+            pesanBerhasil "Data Pengguna " & txtNama.Text & " Berhasil Diupdate"
+        End If
+    Else
+        MsgBox "Silahkan Pilih Data Pengguna yang Akan Diupdate di Table Data!", vbInformation, "Informasi"
+    End If
 End Sub
 
-Private Sub DataGrid1_Click()
-    txtId.Text = CStr(DataGrid1.Columns(0).Value)
-    txtNama.Text = CStr(DataGrid1.Columns(1).Value)
-    txtUsername.Text = CStr(DataGrid1.Columns(3).Value)
-    txtRole.Text = CStr(DataGrid1.Columns(3).Value)
-    id_outlet = CInt(DataGrid1.Columns(4).Value)
-    Combo1.Text = CStr(DataGrid1.Columns(5).Value)
+Private Sub DGPengguna_Click()
+    IdUser = DGPengguna.Columns(0).Value
+    txtNama.Text = CStr(DGPengguna.Columns(1).Value)
+    txtUsername.Text = CStr(DGPengguna.Columns(2).Value)
+    txtEmail.Text = CStr(DGPengguna.Columns(3).Value)
+    cmbRole.Text = CStr(DGPengguna.Columns(4).Value)
+End Sub
+
+Public Sub ambilDataPengguna()
+    adodcPengguna.RecordSource = "SELECT * FROM tb_user"
+    adodcPengguna.Refresh
+    
+    Set DGPengguna.DataSource = adodcPengguna
 End Sub
 
 Private Sub Form_Load()
-    con.Open "Provider=MSDASQL.1;Persist security info=false; Data Source=londi"
+    ambilDataPengguna
+    cmbRole.AddItem "Admin", 0
+    cmbRole.AddItem "Kasir", 1
+    cmbRole.AddItem "Owner", 2
     
-    Call Show_Data
-    
-    Dim rs As New ADODB.Recordset
-    Set rs = New ADODB.Recordset
-    
-    rs.Open "SELECT * FROM tb_outlet", con
-    
-    While Not rs.EOF
-        Combo1.AddItem rs("nama").Value
-        Combo1.ItemData(Combo1.NewIndex) = rs("id").Value
-        rs.MoveNext
-    Wend
-    
-    rs.Close
-    Set rs = Nothing
+    Conn.Open adodcPengguna.ConnectionString
+    IdUser = 0
 End Sub
 
-Private Sub hapusBtn_Click()
-    Call Hapus_Data
+Private Sub Form_Unload(Cancel As Integer)
+    Conn.Close
 End Sub
 
-Private Sub newBtn_Click()
-    Call Clear_Form
-End Sub
-
-Private Sub saveBtn_Click()
-    Call Tambah_Data
-End Sub
-
-Private Sub updateBtn_Click()
-    Call Update_Data
+Private Sub txtCari_Change()
+    If Not txtCari.Text = "" Then
+        cmdCari.Caption = "BATAL"
+        adodcPengguna.RecordSource = "SELECT * FROM tb_user WHERE nama LIKE '%' + '" & txtCari.Text & "' + '%' OR username LIKE '%' + '" & txtCari.Text & "' + '%' OR email LIKE '%' + '" & txtCari.Text & "' + '%'"
+        
+        adodcPengguna.Refresh
+    Else
+        cmdCari.Caption = "CARI"
+        ambilDataPengguna
+    End If
 End Sub
